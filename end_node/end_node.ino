81b0b5bd912899cf2a7983b0791d602df2d04e2a
Hisni <hisnionline@gmail.com>
2019-11-24 15:50:15 +0530

    endnode completed

--- end_node/end_node.ino
+++ end_node/end_node.ino
@@ -1,14 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include "DHT.h"
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+#include "DHT.h"
 
+//DHT Temperature Sensor init
 #define DHTPIN D2       //DHT sensor Connected to D2
-#define DHTTYPE DHT22   //DHT 22  (AM2302), AM2321
+#define DHTTYPE DHT22   //DHT 22 (AM2302), AM2321
 
+//BMP Pressure Sensor init
 #define BMP_SCK 13
 #define BMP_MISO 12
 #define BMP_MOSI 11 
@@ -16,6 +18,7 @@
 
 Adafruit_BMP280 bme; // I2C
 
+//MPU6050 Gyro init
 // MPU6050 Slave Device Address
 const uint8_t MPU6050SlaveAddress = 0x68;
 // Select SDA and SCL pins for I2C communication 
@@ -74,33 +77,18 @@ void setup() {
   Serial.println();
   Serial.println("**************************");
   Serial.println("******** BEGIN ***********");
-  Serial.println("Start Sensors");
+  Serial.println("***** START SSENSORS *****");
+  Serial.println("**************************");
   Serial.println();
 
   Wire.begin(sda, scl);
-
   dht.begin();
   bme.begin();
   MPU6050_Init();
 
   delay(500);
   
-//  Serial.println("- set ESP STA mode");
-//  WiFi.mode(WIFI_STA);
-//  Serial.println("- connecting to wifi");
-//  WiFi.config(ip, gateway, subnet); 
-//  WiFi.begin(ssid, password);
-//  Serial.println("");
-//  while (WiFi.status() != WL_CONNECTED) {
-//    if(counter > 20){
-//       Serial.println("- can't connect, going to sleep");    
-//       hibernate(failConnectRetryInterval);
-//    }
-//    delay(500);
-//    Serial.print(".");
-//    counter++;
-//  }
-//  Serial.println("- wifi connected");
+  connectWIFI();
 
 }
 
@@ -110,99 +98,38 @@ void loop(){
   readBMPSensor();
   processData();
   printData();
-  
-//  Serial.println("- build DATA stream string");
-//  buildDataStream();
-//
-//  Serial.println("- send GET request");
-//  sendHttpRequest();
 
-  delay(5000);
-//  Serial.println();
-//  Serial.println("- got back to sleep");
-//  Serial.println("**************************");
-//  Serial.println("**************************"); 
-//  //hibernate(sleepInterval);
-}
-
-void printData(){
-
-//  Serial.print("DHT ");
-//  Serial.print("temperature read : "+String(t));
-//  Serial.println("  humidity read : "+String(h));
-//
-//  Serial.print("BMP ");
-//  Serial.print("temperature read : "+String(temp));
-//  Serial.print("  Pressure read : "+String(p));
-//  Serial.println("  Altitude read : "+String(a));
-//  
-//  Serial.print("Gyro ");
-//  Serial.print("Ax: "); Serial.print(Ax);
-//  Serial.print(" Ay: "); Serial.print(Ay);
-//  Serial.print(" Az: "); Serial.print(Az);
-//  Serial.print(" T: "); Serial.print(T);
-//  Serial.print(" Gx: "); Serial.print(Gx);
-//  Serial.print(" Gy: "); Serial.print(Gy);
-//  Serial.print(" Gz: "); Serial.println(Gz);
-
-    Serial.print("Temperature : "+String(temperature));
-    Serial.print(" | Humidity : "+String(h)); 
-    Serial.println(" | Pressure : "+String(p)); 
-
-    Serial.print("Gyro ");
-    Serial.print("Ax: "); Serial.print(Ax);
-    Serial.print(" Ay: "); Serial.print(Ay);
-    Serial.print(" Az: "); Serial.print(Az);
-    Serial.print(" T: "); Serial.print(T);
-    Serial.print(" Gx: "); Serial.print(Gx);
-    Serial.print(" Gy: "); Serial.print(Gy);
-    Serial.print(" Gz: "); Serial.println(Gz);
-
-    Serial.print("Stability: "); Serial.print(st);
-    
-    Serial.println();
-}
-
-void readDHTSensor() {
-  h = dht.readHumidity();
-  t = dht.readTemperature();
-  if (isnan(h) || isnan(t)) {
-    t = 0.00;
-    h = 0.00;
-  }
-}
-
-void readBMPSensor(){
-  temp = bme.readTemperature();
-  p = bme.readPressure();
-  a = bme.readAltitude(1013.25);
-  if (isnan(temp) || isnan(p)) {
-    temp = 0.00;
-    p = 0.00;
-  }
-}
-
-void processData(){
-  temperature = (t+temp)/2;
-
-  if( Ax>1.3 || Ax<.7 || Ay>-.1 || Ay<-.7 || Az>-.7 || Az<-103 ){
-    st="Not Stable";
-  }else{
-    st="Stable";
+  if(WiFi.status() != WL_CONNECTED){
+    connectWIFI();
   }
   
+  Serial.println("- build DATA stream string");
+  buildDataStream();
+
+  Serial.println("- send POST request");
+  sendHttpRequest();
+
+  delay(10000);
+//  hibernate(sleepInterval);
 }
 
-void readMPUSensor(){
-  Read_RawValue(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_XOUT_H);
-  //divide each with their sensitivity scale factor
-  Ax = (double)AccelX/AccelScaleFactor;
-  Ay = (double)AccelY/AccelScaleFactor;
-  Az = (double)AccelZ/AccelScaleFactor;
-  T = (double)Temperature/340+36.53; //temperature formula
-  Gx = (double)GyroX/GyroScaleFactor;
-  Gy = (double)GyroY/GyroScaleFactor;
-  Gz = (double)GyroZ/GyroScaleFactor;
+void connectWIFI(){
+  Serial.println("- set ESP STA mode");
+  WiFi.mode(WIFI_STA);
+  Serial.println("- connecting to wifi");
+  WiFi.config(ip, gateway, subnet); 
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  while (WiFi.status() != WL_CONNECTED) {
+    if(counter > 20){
+       Serial.println("- can't connect, going to sleep");    
+       hibernate(failConnectRetryInterval);
+    }
+    delay(500);
+    Serial.print(".");
+    counter++;
+  }
+  Serial.println("- wifi connected");
 }
 
 void buildDataStream() {
@@ -226,13 +153,68 @@ void sendHttpRequest() {
   http.end();
 }
 
-void hibernate(int pInterval) {
-  WiFi.disconnect();
-  ESP.deepSleep(10 * 600000 * pInterval, WAKE_RFCAL);
-  delay(100);
+void printData(){
+  
+    Serial.print("Temperature : "+String(temperature));
+    Serial.print(" | Humidity : "+String(h)); 
+    Serial.println(" | Pressure : "+String(p)); 
+
+    Serial.print("Gyro ");
+    Serial.print("Ax: "); Serial.print(Ax);
+    Serial.print(" Ay: "); Serial.print(Ay);
+    Serial.print(" Az: "); Serial.print(Az);
+    Serial.print(" T: "); Serial.print(T);
+    Serial.print(" Gx: "); Serial.print(Gx);
+    Serial.print(" Gy: "); Serial.print(Gy);
+    Serial.print(" Gz: "); Serial.println(Gz);
+
+    Serial.print("Stability: "); Serial.print(st);
+    
+    Serial.println();
 }
 
-// read all 14 register
+void processData(){
+  temperature = (t+temp)/2;
+
+  if( Ax>1.3 || Ax<.7 || Ay>-.1 || Ay<-.7 || Az>-.7 || Az<-103 ){
+    st="Not Stable";
+  }else{
+    st="Stable";
+  }
+}
+
+void readDHTSensor() {
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    t = 0.00;
+    h = 0.00;
+  }
+}
+
+void readBMPSensor(){
+  temp = bme.readTemperature();
+  p = bme.readPressure();
+  a = bme.readAltitude(1013.25);
+  if (isnan(temp) || isnan(p)) {
+    temp = 0.00;
+    p = 0.00;
+  }
+}
+
+void readMPUSensor(){
+  Read_RawValue(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_XOUT_H);
+  //divide each with their sensitivity scale factor
+  Ax = (double)AccelX/AccelScaleFactor;
+  Ay = (double)AccelY/AccelScaleFactor;
+  Az = (double)AccelZ/AccelScaleFactor;
+  T = (double)Temperature/340+36.53; //temperature formula
+  Gx = (double)GyroX/GyroScaleFactor;
+  Gy = (double)GyroY/GyroScaleFactor;
+  Gz = (double)GyroZ/GyroScaleFactor;
+}
+
+//read all 14 register
 void Read_RawValue(uint8_t deviceAddress, uint8_t regAddress){
   Wire.beginTransmission(deviceAddress);
   Wire.write(regAddress);
@@ -267,4 +249,10 @@ void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data){
   Wire.write(regAddress);
   Wire.write(data);
   Wire.endTransmission();
+}
+
+void hibernate(int pInterval) {
+  WiFi.disconnect();
+  ESP.deepSleep(10 * 600000 * pInterval, WAKE_RFCAL);
+  delay(100);
 }
